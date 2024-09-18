ed8243d0150e63a4cd880774a7ea6d8874d8b7d1
Yabiee-5 <e205744@ie.u-ryukyu.ac.jp>
2024-09-18 20:25:36 +0900

    fst

---
+++ api/schemas/task.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        from_attributes=True
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        from_attributes=True
